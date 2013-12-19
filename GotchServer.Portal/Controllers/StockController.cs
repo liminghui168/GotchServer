cb90ef47ef019f5737850ec82e282fd16452ccd8
home-pc <liminghui168@qq.com>
2013-12-19 22:14:23 +0800

    first pre next last

--- GotchServer.Portal/Controllers/StockController.cs
+++ GotchServer.Portal/Controllers/StockController.cs
@@ -14,7 +14,11 @@ namespace GotchServer.Portal.Controllers
 
         private readonly StockRepostory stockRepostory = new StockRepostory();
 
-
+        #region 分页获取数据
+        /// <summary>
+        /// 分页获取数据
+        /// </summary>
+        /// <returns></returns>
         public ActionResult LoadList()
         {
             int pageIndex = Request["pageIndex"] == null ? 1 : int.Parse(Request["pageIndex"]);
@@ -26,7 +30,7 @@ namespace GotchServer.Portal.Controllers
             var data = new
                            {
                                total = total,
-                               rows = from stock in list  
+                               rows = from stock in list
                                       select new
                                                  {
                                                      stock.Id,
@@ -38,8 +42,14 @@ namespace GotchServer.Portal.Controllers
                            };
 
             return Json(data, JsonRequestBehavior.AllowGet);
-        }
+        } 
+        #endregion
 
+        #region 添加
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <returns></returns>
         public ActionResult AddStockDetail()
         {
             string json = Request["stockJson"];
@@ -49,28 +59,41 @@ namespace GotchServer.Portal.Controllers
             if (result > 0)
                 return Content("ok");
             return Content("error");
-        }
+        } 
+        #endregion
 
+        #region 更新
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <returns></returns>
         [System.Web.Mvc.HttpPost]
         public ActionResult UpdateStockDetail()
         {
             string json = Request["stockJson"];
             Stock stock = new Stock();
             stock = Newtonsoft.Json.JsonConvert.DeserializeObject<Stock>(json);
-            int result =  stockRepostory.UpdateStock(stock);
+            int result = stockRepostory.UpdateStock(stock);
             if (result > 0)
                 return Content("ok");
             return Content("error");
-        }
+        } 
+        #endregion
 
+        #region 删除
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult DeleteStockDetail(int id)
         {
             int result = 0;
             var stockModel = stockRepostory.LoadStocks(s => s.Id == id).FirstOrDefault();
-            if(stockModel!=null)
+            if (stockModel != null)
             {
                 result = stockRepostory.DeleteStock(stockModel);
-                if(result>0)
+                if (result > 0)
                 {
                     return Content("ok");
                 }
@@ -78,13 +101,198 @@ namespace GotchServer.Portal.Controllers
             }
 
             return Content("error");
+        } 
+        #endregion
+
+        
+
+        #region 获取第一条数据
+        /// <summary>
+        /// 获取第一条数据
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LoadStockDetailFirst()
+        {
+            var stockModel = stockRepostory.LoadStocks(s => true).FirstOrDefault();
+            var data = new
+            {
+                stockModel.Id,
+                stockModel.stock1,
+                stockModel.vendor,
+                stockModel.vendorname,
+                stockModel.vref,
+                stockModel.des,
+                stockModel.shortdes,
+                stockModel.power,
+                stockModel.opowerrms,
+                stockModel.opowerpmpo,
+                stockModel.partno,
+                stockModel.unit,
+                stockModel.pricecurr,
+                stockModel.price,
+                stockModel.costcurr,
+                stockModel.cost,
+                createdata = stockModel.createdate.Value.ToString("yyyy-MM-dd"),
+                stockModel.brand,
+                stockModel.category,
+                stockModel.gbbarcode,
+                stockModel.ocbarcode,
+                stockModel.pbarcode,
+                stockModel.sclass,
+                stockModel.subclass,
+                stockModel.active
+            };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
+        #endregion
 
+        #region 获取上一条数据
+        /// <summary>
+        ///  获取上一条数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult LoadStockDetailPre(int id)
+        {
+            var stockModel = stockRepostory.LoadStocks(s => s.Id < id).LastOrDefault();
+            if (stockModel != null)
+            {
+                var data = new
+                {
+                    stockModel.Id,
+                    stockModel.stock1,
+                    stockModel.vendor,
+                    stockModel.vendorname,
+                    stockModel.vref,
+                    stockModel.des,
+                    stockModel.shortdes,
+                    stockModel.power,
+                    stockModel.opowerrms,
+                    stockModel.opowerpmpo,
+                    stockModel.partno,
+                    stockModel.unit,
+                    stockModel.pricecurr,
+                    stockModel.price,
+                    stockModel.costcurr,
+                    stockModel.cost,
+                    createdata = stockModel.createdate.Value.ToString("yyyy-MM-dd"),
+                    stockModel.brand,
+                    stockModel.category,
+                    stockModel.gbbarcode,
+                    stockModel.ocbarcode,
+                    stockModel.pbarcode,
+                    stockModel.sclass,
+                    stockModel.subclass,
+                    stockModel.active
+                };
 
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+
+            //已经是第一条数据
+            return Content("isFirst");
+        } 
+        #endregion
+
+        #region 获取下一条数据
+        /// <summary>
+        /// 获取下一条数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult LoadStockDetailNext(int id)
+        {
+            var stockModel = stockRepostory.LoadStocks(s => s.Id > id).FirstOrDefault();
+            if (stockModel != null)
+            {
+                var data = new
+                {
+                    stockModel.Id,
+                    stockModel.stock1,
+                    stockModel.vendor,
+                    stockModel.vendorname,
+                    stockModel.vref,
+                    stockModel.des,
+                    stockModel.shortdes,
+                    stockModel.power,
+                    stockModel.opowerrms,
+                    stockModel.opowerpmpo,
+                    stockModel.partno,
+                    stockModel.unit,
+                    stockModel.pricecurr,
+                    stockModel.price,
+                    stockModel.costcurr,
+                    stockModel.cost,
+                    createdata = stockModel.createdate.Value.ToString("yyyy-MM-dd"),
+                    stockModel.brand,
+                    stockModel.category,
+                    stockModel.gbbarcode,
+                    stockModel.ocbarcode,
+                    stockModel.pbarcode,
+                    stockModel.sclass,
+                    stockModel.subclass,
+                    stockModel.active
+                };
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+
+            //已经是最后一条数据了
+            return Content("isLast");
+        } 
+        #endregion
+
+        #region 获取最后一条数据
+        /// <summary>
+        /// 获取最后一条数据
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LoadStockDetailLast()
+        {
+            var stockModel = stockRepostory.LoadStocks(s => true).LastOrDefault();
+            var data = new
+            {
+                stockModel.Id,
+                stockModel.stock1,
+                stockModel.vendor,
+                stockModel.vendorname,
+                stockModel.vref,
+                stockModel.des,
+                stockModel.shortdes,
+                stockModel.power,
+                stockModel.opowerrms,
+                stockModel.opowerpmpo,
+                stockModel.partno,
+                stockModel.unit,
+                stockModel.pricecurr,
+                stockModel.price,
+                stockModel.costcurr,
+                stockModel.cost,
+                createdata = stockModel.createdate.Value.ToString("yyyy-MM-dd"),
+                stockModel.brand,
+                stockModel.category,
+                stockModel.gbbarcode,
+                stockModel.ocbarcode,
+                stockModel.pbarcode,
+                stockModel.sclass,
+                stockModel.subclass,
+                stockModel.active
+            };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        #endregion
+
+        #region 获取详细信息
+        /// <summary>
+        /// 获取详细信息
+        /// </summary>
+        /// <returns></returns>
         public ActionResult LoadStockDetail(int id)
         {
             var stockModel = stockRepostory.LoadStocks(s => s.Id == id).FirstOrDefault();
-            if(stockModel==null)
+            if (stockModel == null)
             {
                 return Content("null");
             }
@@ -117,33 +325,9 @@ namespace GotchServer.Portal.Controllers
                                stockModel.active
                            };
 
-            return Json(data,JsonRequestBehavior.AllowGet);
-        }
-
-        //public IEnumerable<Stock> Get()
-        //{
-        //    IQueryable<Stock> list = null;
-        //    int pageIndex = 1;
-        //    int pageSize = 10;
-
-        //    int total = 0;
-
-        //    list = stockRepostory.LoadStocksByPage(pageIndex, pageSize, out total, s => true, s => s.Id, true);
-
-        //    return list;
-        //}
-
-        //public IEnumerable<Stock> Post(int pageIndex)
-        //{
-        //    return null;
-        //}
-
-        //[HttpPost]
-        //[ActionName("comlpx")]
-        //public IEnumerable<Stock> LoadStocks([FromBody] int pageIndex, [FromBody] int pageSize)
-        //{
-        //    return null;
-        //}
+            return Json(data, JsonRequestBehavior.AllowGet);
+        } 
+        #endregion
 
     }
 }
